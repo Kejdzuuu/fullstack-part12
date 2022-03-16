efd27c82d68e23115b92277d3d333ea784c5af5c
Maciej Modrzejewski <kejdzuuu@gmail.com>
2022-03-16 20:54:36 +0100

    Ex. 12.10

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const { Todo } = require("../mongo");
+const redis = require("../redis");
 const router = express.Router();
+
+const redis_todos_key = "added_todos";
 
 /* GET todos listing. */
 router.get("/", async (_, res) => {
   const todos = await Todo.find({});
   res.send(todos);
+});
+
+router.get("/statistics", async (_, res) => {
+  const value = (await redis.getAsync(redis_todos_key)) ?? 0;
+  const response = {
+    added_todos: parseInt(value),
+  };
+  res.json(response);
 });
 
 /* POST todo to listing. */
@@ -14,6 +25,8 @@ router.post("/", async (req, res) => {
     text: req.body.text,
     done: false,
   });
+  const value = (await redis.getAsync(redis_todos_key)) ?? 0;
+  await redis.setAsync(redis_todos_key, value + 1);
   res.send(todo);
 });
 
